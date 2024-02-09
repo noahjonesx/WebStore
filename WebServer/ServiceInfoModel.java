6f997e7c403d20ed78bd3db15aa331d2b886bdb3
cryptic <71420972+noahjonesx@users.noreply.github.com>
2024-02-09 10:16:45 -0600

    Add files via upload

---
+++ WebServer/ServiceInfoModel.java
@@ -0,0 +1,13 @@
+public class ServiceInfoModel {
+    public static int PRODUCT_INFO_SERVICE = 1;
+    public static int ORDER_INFO_SERVICE = 2;
+    public static int PRODUCT_PRICE_UPDATE_SERVICE = 3;
+
+    public static int PRODUCT_QUANTITY_UPDATE_SERVICE = 4;
+
+    public int serviceCode;
+    public String serviceHostAddress;
+
+    public int serviceHostPort;
+
+}
